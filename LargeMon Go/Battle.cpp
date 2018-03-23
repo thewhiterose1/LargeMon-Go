cb0890094fed6df242a4d2d3e9d7b2bc52ef6db4
Jeremy <jjhotham@gmail.com>
2018-03-23 02:02:01 +0000

    battle additions :)

---
+++ LargeMon Go/Battle.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "Battle.h"
+
+
+Battle::Battle()
+{
+	int i = 0;
+}
+
+
+Battle::~Battle()
+{
+}
---
+++ LargeMon Go/Battle.h
@@ -0,0 +1,8 @@
+#pragma once
+class Battle
+{
+public:
+	Battle();
+	~Battle();
+};
+
